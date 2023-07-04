b452e203f01709b465f0c310f2ae86b92245112c
James Tripp <james.tripp@warwick.ac.uk>
2023-07-04 13:53:17 +0000

    Prettier message and correct adding to .bashrc

--- scripts/add_to_bashrc.sh
+++ scripts/add_to_bashrc.sh
@@ -2,17 +2,17 @@ export BUILDDIR='/workspaces/r-dev-env/bin/R'
 export TOP_SRCDIR='/workspaces/r-dev-env/Downloads/R'
 
 echo "
-Welcome to the Containerised R Development Environment\n
-\n
-Here you can modify, compile and use R as described in\n
-the r development guide:\n
-    https://contributor.r-project.org/rdevguide/GetStart.html#building-r\n
-\n
-\n
-To help you, we have already set the following environment variables:\n
-\n
-BUILDIR = "$BUILDDIR"\n
-TOP_SRCDIR = "$TOP_SRCDIR"\n
-\n
+Welcome to the Containerised R Development Environment
+
+Here you can modify, compile and use R as described in
+the r development guide:
+    https://contributor.r-project.org/rdevguide/GetStart.html#building-r
+
+
+To help you, we have already set the following environment variables:
+
+BUILDIR = "$BUILDDIR"
+TOP_SRCDIR = "$TOP_SRCDIR"
+
 Have fun :)
 "
