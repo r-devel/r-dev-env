bc303b653a2fcf11e931d1a41d002b03592dcd47
James Tripp <james.tripp@warwick.ac.uk>
2023-07-04 14:15:49 +0100

    Attempt at message printing out env variables

---
+++ scripts/add_to_bashrc.sh
@@ -0,0 +1,18 @@
+export BUILDDIR='/workspaces/r-dev-env/bin/R'
+export TOP_SRCDIR='/workspaces/r-dev-env/Downloads/R'
+
+echo "
+Welcome to the Containerised R Development Environment\n
+\n
+Here you can modify, compile and use R as described in\n
+the r development guide:\n
+    https://contributor.r-project.org/rdevguide/GetStart.html#building-r\n
+\n
+\n
+To help you, we have already set the following environment variables:\n
+\n
+BUILDIR = "$BUILDDIR"\n
+TOP_SRCDIR = "$TOP_SRCDIR"\n
+\n
+Have fun :)
+"
