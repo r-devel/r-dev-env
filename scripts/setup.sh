953dbb0f9416f4040fb49d933d7b5b5868c553a0
Atharva Shirdhankar <72031540+StarTrooper08@users.noreply.github.com>
2023-07-11 20:36:19 +0000

    simplified setup script file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
-R_VERSION="R-4-2-3"
-#SOURCE_DIR="/home/vscode/r_source_code"
-#BUILD_DIR="/home/vscode/r_build"
 
-R_VERSION_SOURCE_DIR=$TOP_SRCDIR/$R_VERSION
-R_VERSION_BUILD_DIR=$BUILDDIR/$R_VERSION
+#ENV VAR ($BUILDDIR & $TOP_SRCDIR) are created inside docker container itself.
 
-mkdir -p $R_VERSION_SOURCE_DIR
-mkdir -p $R_VERSION_BUILD_DIR
+svn checkout https://svn.r-project.org/R/tags/ "$TOP_SRCDIR"
 
-svn checkout https://svn.r-project.org/R/tags/$R_VERSION/ "$R_VERSION_SOURCE_DIR"
+cd "$BUILDDIR"
 
-
-cd $R_VERSION_BUILD_DIR
-
-#./configure" --prefix=$R_VERSION_BUILD_DIR --enable-R-shlib --without-recommended-packages
-"$R_VERSION_SOURCE_DIR/configure" --enable-R-shlib --without-recommended-packages
+"$TOP_SRCDIR/configure" --enable-R-shlib --without-recommended-packages
 
 make check
 make
 sudo make install
-
 
 #After setup process(be in the build_dir for this process)
 #Run R to test whats the bug and what output we are getting
@@ -35,21 +25,3 @@ sudo make install
 #run svn diff > path.diff
 #cd $BUILDDIR
 #make check
-
-# create sys links for new binaries
-BINARIES=$R_VERSION_BUILD_DIR/bin/*
-
-for f in $BINARIES
-do
-if [[ "$f" != *\.* ]]
-then
-binary_name="$(basename -- $f)"
-source=$R_VERSION_BUILD_DIR/bin/$binary_name 
-target=/usr/local/bin/$binary_name
-sudo rm -f $target
-sudo ln -s $source $target
-fi
-done
-
-# install language server in new R version
-Rscript -e "install.packages('languageserver', repos='https://cran.rstudio.com')"
