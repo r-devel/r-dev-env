1e1b29561c71f49a5141f87a24b2d23f363248c0
Atharva Shirdhankar <72031540+StarTrooper08@users.noreply.github.com>
2023-08-24 14:28:12 +0530

    added multi_r script to run from anywhere

--- scripts/localscript.sh
+++ scripts/localscript.sh
@@ -3,5 +3,7 @@
 mkdir -p /home/vscode/.local/bin
 
 chmod +x /workspaces/r-dev-env/scripts/rterm.sh
+chmod +x /workspaces/r-dev-env/scripts/multi_r.sh
 
 mv /workspaces/r-dev-env/scripts/rterm.sh /home/vscode/.local/bin/rterm
+cp /workspaces/r-dev-env/scripts/multi_r.sh /home/vscode/.local/bin/multi_r
