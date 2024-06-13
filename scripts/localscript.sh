5a7dd7d4b661237f15564bf905e0d46bdbc81d03
Heather Turner <ht@heatherturner.net>
2024-06-13 09:09:36 +0100

    remove scripts that are no longer used

--- scripts/localscript.sh
+++ scripts/localscript.sh
@@ -3,12 +3,9 @@
 mkdir -p /home/vscode/.local/bin
 mkdir -p $PATCHDIR
 
-chmod +x /workspaces/r-dev-env/scripts/rterm.sh
-
 chmod +x /workspaces/r-dev-env/scripts/which_r.sh
 chmod +x /workspaces/r-dev-env/scripts/set_build_r.sh
 
-mv /workspaces/r-dev-env/scripts/rterm.sh /home/vscode/.local/bin/rterm
 cp /workspaces/r-dev-env/scripts/which_r.sh /home/vscode/.local/bin/which_r
 cp /workspaces/r-dev-env/scripts/set_build_r.sh /home/vscode/.local/bin/set_build_r
 
