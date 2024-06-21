f11a70ab9e7f7a5a93695995751dd39aa6f213c4
Atharva <ashirdhankar458@gmail.com>
2024-06-21 16:51:34 +0530

    fix: removed copying set build r script from localscript file

--- scripts/localscript.sh
+++ scripts/localscript.sh
@@ -7,7 +7,7 @@ chmod +x /workspaces/r-dev-env/scripts/which_r.sh
 chmod +x /workspaces/r-dev-env/scripts/set_build_r.sh
 
 cp /workspaces/r-dev-env/scripts/which_r.sh /home/vscode/.local/bin/which_r
-cp /workspaces/r-dev-env/scripts/set_build_r.sh /home/vscode/.local/bin/set_build_r
+# cp /workspaces/r-dev-env/scripts/set_build_r.sh /home/vscode/.local/bin/set_build_r
 
 # remove git directory
 rm -rf .git
