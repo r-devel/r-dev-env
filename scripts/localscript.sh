f6d8922431835095dbdb1ec9c9a8fe8817c5092f
Heather Turner <ht@heatherturner.net>
2024-07-26 09:44:51 +0100

    remove old code designed for scripts not functions

--- scripts/localscript.sh
+++ scripts/localscript.sh
@@ -4,30 +4,17 @@
 # Workspace root is $PWD at the start
 WORK_DIR=$PWD
 
-# Define patch directory in workspace root ($PWD at start)
+# Create patch directory in workspace root ($PWD at start)
 PATCHDIR="$WORK_DIR/patches"
-
-# Define directory where bash will look for scripts
-SCRIPT_DIR="$HOME/.local/bin"
-
-# Create necessary directories
-mkdir -p $SCRIPT_DIR
 mkdir -p $PATCHDIR
 
-# Set executable permissions for the scripts
-chmod +x $WORK_DIR/scripts/which_r.sh
-chmod +x $WORK_DIR/scripts/set_build_r.sh
-
-# Copy over the scripts from the workspace directory to the script directory
-cp $WORK_DIR/scripts/which_r.sh $SCRIPT_DIR/which_r
-cp $WORK_DIR/scripts/set_build_r.sh $SCRIPT_DIR/set_build_r
+# Copy the which_r and set_build_r function definitions to .bashrc
+cat $WORK_DIR/scripts/which_r.sh >> ~/.bashrc
+cat $WORK_DIR/scripts/set_build_r.sh >> ~/.bashrc
 
 # Copy over the welcome message script to be run when bash terminal starts
 cat $WORK_DIR/scripts/welcome_msg.sh >> ~/.bashrc
 
-# Following not needed?
-#cat $WORK_DIR/scripts/which_r.sh >> ~/.bashrc
-#cat $WORK_DIR/scripts/set_build_r.sh >> ~/.bashrc
 #bash ~/.bashrc
 
 # Remove git directory if it exists
