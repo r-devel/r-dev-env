f40368164aaaa3f22eac3650fb985daa96d1381b
Atharva <ashirdhankar458@gmail.com>
2024-07-29 01:43:07 +0530

    update: copying vscode settings from devcontainer json to vscode settings using jq

--- scripts/localscript.sh
+++ scripts/localscript.sh
@@ -1,8 +1,12 @@
 # This script is run after the workspace is created
 #!/bin/bash
 
+local_script(){
+
 # Workspace root is $PWD at the start
 WORK_DIR=$PWD
+VSCODE_DIR="$WORK_DIR/.vscode"
+DEVCONTAINER_JSON="$WORK_DIR/.devcontainer/devcontainer.json"
 
 # Create patch directory in workspace root ($PWD at start)
 PATCHDIR="$WORK_DIR/patches"
@@ -19,3 +23,15 @@ cat $WORK_DIR/scripts/welcome_msg.sh >> ~/.bashrc
 
 # Remove git directory if it exists
 rm -rf .git
+
+# copying vscode extension settings from devcontainer json to vscode settings json using jq
+if [ -f "$DEVCONTAINER_JSON" ]; then
+        jq '.customizations.vscode.settings' "$DEVCONTAINER_JSON" > "$VSCODE_DIR/settings.json"
+fi
+
+
+}
+
+
+# Run the main function
+local_script
