aa3881daaf68e882f2695f4c20b84aa6b8e83493
avinab-neogy <avinabneogy23@gmail.com>
2025-07-31 10:11:49 +0530

    modified local script

--- scripts/localscript.sh
+++ scripts/localscript.sh
@@ -24,7 +24,6 @@ cat $SCRIPTS_DIR/set_build_r.sh >> ~/.bashrc
 cat $SCRIPTS_DIR/welcome_msg.sh >> ~/.bashrc
 
 #bash ~/.bashrc
-
 # Remove git directory if it exists
 rm -rf .git
 
@@ -42,7 +41,6 @@ local_script
 
 # Remove the .vscode/settings.json file if it exists so that
 # it does not interfere with the devcontainer.json
-<<<<<<< HEAD
 rm -f "$WORK_DIR/.vscode/settings.json"
 
 # 1. Build the ptrace helper library
@@ -50,6 +48,3 @@ gcc -shared -fPIC -o "$SCRIPTS_DIR/allow_ptrace.so" "$SCRIPTS_DIR/allow_ptrace.c
 
 # 2. Mark the wrapper executable
 chmod +x "$SCRIPTS_DIR/launch_r.sh"
-=======
-rm -f .vscode/settings.json
->>>>>>> 964acc0 ([pre-commit.ci] Fixing issues with pre-commit)
