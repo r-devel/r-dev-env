47fedb03eaa3de2482a8a3884c3bf00090133a08
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-08-11 19:18:19 +0100

    Update command

--- scripts/manifestR.R
+++ scripts/manifestR.R
@@ -204,7 +204,7 @@ manifest_string <- function() {
 
 # Docker functions
 docker_start <- function() {
-  args <- system2("docker", args = c("run", "-it -d",  docker_id),
+  args <- system2("docker", args = c("run", "-i -d",  docker_id),
                                      stdout = TRUE)
   args
 }
@@ -214,7 +214,7 @@ docker_stop <- function() {
 }
 
 docker_command <- function(arguments) {
-  docker <- paste("run", "-it",  docker_id, "bash", "-c",
+  docker <- paste("exec", "-i",  docker_id, "bash", "-c",
                   sep = " ", collapse = "")
 
   a <- paste('"', paste(arguments, sep = " ", collapse = " "), '"',
